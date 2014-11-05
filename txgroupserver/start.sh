681212774066e00d14b1168badc22c2bb4258382
Carl Waldbieser <waldbiec@lafayette.edu>
2014-11-05 11:20:55 -0500

    Explicilty specify PID file location in startup script.

--- txgroupserver/start.sh
+++ txgroupserver/start.sh
@@ -5,6 +5,7 @@ SCRIPT="$THISDIR/$(basename $0)"
 PYENV="$THISDIR/pyenv"
 GROUPER_USER=grouper
 LOGFILE=/var/log/txgroupserver/txgroupserver.log
+PIDFILE="$THISDIR/twistd.pid"
 
 . "$PYENV/bin/activate"
-twistd -u $(id -u "$GROUPER_USER") -g$(id -g "$GROUPER_USER") -y "$THISDIR/txgroupserver.py" -l "$LOGFILE"
+twistd -u $(id -u "$GROUPER_USER") -g$(id -g "$GROUPER_USER") -y "$THISDIR/txgroupserver.py" -l "$LOGFILE" --pidfile "$PIDFILE"
