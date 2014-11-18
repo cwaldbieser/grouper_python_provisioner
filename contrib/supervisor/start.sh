bf9c21541a18dad57eb525e37c483d2dd8ca2a78
Carl Waldbieser <waldbiec@lafayette.edu>
2014-11-18 13:40:17 -0500

    Updated init scripts to reflect changes in file system organization.

--- contrib/supervisor/start.sh
+++ contrib/supervisor/start.sh
@@ -3,7 +3,7 @@
 THISDIR="$( cd $(dirname $0); pwd)"
 SCRIPT="$THISDIR/$(basename $0)"
 PYENV="$THISDIR/pyenv"
-CONFIG="$THISDIR/supervisord.conf"
+CONFIG="/etc/supervisord.conf"
 
 . "$PYENV/bin/activate"
 cd "$THISDIR"
