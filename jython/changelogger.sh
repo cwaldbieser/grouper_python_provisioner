150e637fe87ff1a5c9abad11af6fd8fded59a36c
Carl Waldbieser <waldbiec@lafayette.edu>
2014-11-18 14:10:46 -0500

    BUGFIX: Specify a Jython cache folder to which the grouper account can write.

--- jython/changelogger.sh
+++ jython/changelogger.sh
@@ -5,9 +5,9 @@ trap "kill -- -$$" EXIT
 THISDIR="$( cd $(dirname $0); pwd)"
 SCRIPT="$THISDIR/$(basename $0)"
 CHANGELOGGER="$THISDIR/process_changelog.py"
-CONFIG="$THISDIR/process_changelog.cfg"
-export GROUPER_HOME=/opt/internet2/grouper/grouper.apiBinary-2.2.0
+export GROUPER_HOME=/opt/internet2/grouper/grouper.apiBinary
 GSH_JYTHON="$GROUPER_HOME/bin/gsh.jython"
+CACHEDIR="$THISDIR/cachedir"
 
 cd "$THISDIR"
-"$GSH_JYTHON" "$CHANGELOGGER" -c "$CONFIG"
+"$GSH_JYTHON" -p "python.cachedir=$CACHEDIR" "$CHANGELOGGER"
