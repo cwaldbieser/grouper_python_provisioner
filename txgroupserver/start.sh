7e1806da659a40f81ec5e1889f51de88749214c1
Carl Waldbieser <waldbiec@lafayette.edu>
2015-02-18 13:34:48 -0500

    Added a web service that can be used to monitor the status of the group server.
    Makes like easy to monitor status via Nagios.  HTTP 200 means OK, any other status
    ought to raise the alarm.

--- txgroupserver/start.sh
+++ txgroupserver/start.sh
@@ -5,7 +5,10 @@ SCRIPT="$THISDIR/$(basename $0)"
 PYENV="$THISDIR/pyenv"
 GROUPER_USER=grouper
 LOGFILE=/var/log/txgroupserver/txgroupserver.log
+STATUS_LOGFILE=/var/log/txgroupserver/status.log
 PIDFILE="$THISDIR/twistd.pid"
+STATUS_PIDFILE="$THISDIR/status.pid"
 
 . "$PYENV/bin/activate"
 twistd -u $(id -u "$GROUPER_USER") -g$(id -g "$GROUPER_USER") -y "$THISDIR/txgroupserver.py" -l "$LOGFILE" --pidfile "$PIDFILE"
+twistd -u $(id -u "$GROUPER_USER") -g$(id -g "$GROUPER_USER") -y "$THISDIR/status2http.py" -l "$STATUS_LOGFILE" --pidfile "$STATUS_PIDFILE"
--- txgroupserver/stop.sh
+++ txgroupserver/stop.sh
@@ -2,14 +2,29 @@
 
 THISDIR="$( cd $(dirname $0); pwd)"
 SCRIPT="$THISDIR/$(basename $0)"
-PIDFILE="$THISDIR/twistd.pid"
 
+PIDFILE="$THISDIR/twistd.pid"
 if [ -f "$PIDFILE" ]; then
     pid=$(cat "$PIDFILE")
     if [ ! -z "$pid" ]; then
         kill -HUP "$pid"
-        exit $?
+        retval=$?
     fi
 fi
-exit 1
+if [ "$retval" -ne 0 ]; then
+    exit 1
+fi
 
+PIDFILE="$THISDIR/status.pid"
+if [ -f "$PIDFILE" ]; then
+    pid=$(cat "$PIDFILE")
+    if [ ! -z "$pid" ]; then
+        kill -HUP "$pid"
+        retval=$?
+    fi
+fi
+if [ "$retval" -ne 0 ]; then
+    exit 1
+fi
+
+exit "$retval"
