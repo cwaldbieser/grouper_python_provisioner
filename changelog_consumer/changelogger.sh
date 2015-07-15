41cedfe5bb3cd3d03d7aa6a132e6497d5e497ebd
Carl Waldbieser <waldbiec@lafayette.edu>
2015-07-15 10:51:54 -0400

    Include the rabbitmq client JAR with the scripts.

--- changelog_consumer/changelogger.sh
+++ changelog_consumer/changelogger.sh
@@ -5,16 +5,16 @@ trap "kill -- -$$" EXIT
 THISDIR="$( cd $(dirname $0); pwd)"
 SCRIPT="$THISDIR/$(basename $0)"
 CHANGELOGGER="$THISDIR/process_changelog.py"
-export GROUPER_HOME=/opt/internet2/grouper/grouper.apiBinary
+GROUPER_HOME=${GROUPER_HOME:-/opt/internet2/grouper/grouper.apiBinary}
+export GROUPER_HOME
 GSH_JYTHON="$GROUPER_HOME/bin/gsh.jython"
 CACHEDIR="$THISDIR/cachedir"
-RABBITMQ_JAR=/home/waldbiec/tarballs/rabbitmq-java-client-bin-3.4.1/rabbitmq-client.jar
+RABBITMQ_JAR="$THISDIR/lib/rabbitmq-client.jar"
 if [ -z "$CLASSPATH" ]; then
     CLASSPATH="$RABBITMQ_JAR"
 else
     CLASSPATH="$CLASSPATH:$RABBITMQ_JAR"
 fi
 export CLASSPATH
-
 cd "$THISDIR"
 "$GSH_JYTHON" -p "python.cachedir=$CACHEDIR" "$CHANGELOGGER"
