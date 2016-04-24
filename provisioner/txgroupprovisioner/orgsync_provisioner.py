d08981214bfffe368119fd4f87d70fb6df1f5f0e
Carl Waldbieser <cwaldbieser@gmail.com>
2016-04-23 21:13:00 -0400

    In-progress Orgsync provisioner.

--- provisioner/txgroupprovisioner/orgsync_provisioner.py
+++ provisioner/txgroupprovisioner/orgsync_provisioner.py
@@ -20,12 +20,25 @@ from interface import (
     IProvisionerFactory,
     IProvisioner,
 )
+from kikimessage import (
+    ADD_ACTION,
+    DELETE_ACTION,
+    UPDATE_ACTION,
+)
 from utils import get_plugin_factory
+
+
+ParsedMessage = namedtuple(
+    'ParsedMessage', 
+    ["action", "subject", "attributes"])
+
+
+class UnknowActionError(Exception):
+    pass
 
 
 class OrgsyncProvisionerFactory(object):
     implements(IPlugin, IProvisionerFactory)
-
     tag = "orgsync"
     opt_help = "Orgsync RESTful API Provisioner"
     opt_usage = "This plugin does not support any options."
@@ -52,7 +65,8 @@ class OrgsyncProvisioner(object):
         try:
             # Load config.
             scp = load_config(config_file, defaults=self.get_config_defaults())
-            config = section2dict(scp, "PROVISIONER")
+            section = "PROVISIONER"
+            config = section2dict(scp, section)
             self.config = config
             # Start logger.
             log_level = config.get('log_level', default_log_level)
@@ -61,6 +75,18 @@ class OrgsyncProvisioner(object):
             log.info("Initializing Orgsync RESTful API provisioner.",
                 event_type='init_provisioner')
             # Load API configuration info-- endpoint info, URL, API key.
+            try:
+                self.endpoint_s = config.get("endpoint", None)
+                self.url_prefix = config["url_prefix"]
+                self.api_key = config["api_key"]
+            except KeyError as ex:
+                raise OptionMissingError(
+                    "A require option was missing: '{0}:{1}'.".format(
+                        section, ex.args[0]))
+        except Exception as ex:
+            d = self.reactor.callLater(0, self.reactor.stop)
+            log.failure("Provisioner failed to initialize: {0}".format(ex))
+            raise
                      
     @inlineCallbacks                                                   
     def provision(self, amqp_message):             
@@ -69,13 +95,55 @@ class OrgsyncProvisioner(object):
         """                                              
         log = self.log
         try:
-            entity = self.parse_message(amqp_message)
+            msg = self.parse_message(amqp_message)
         except Exception as ex:
             log.warn("Error parsing message: {error}", error=ex)
             raise
+        try:
+            if msg.action in (ADD_ACTION, UPDATE_ACTION):
+                yield self.provision_subject(msg)
+            elif msg.action == DELETE_ACTION:
+                yield self.deprovision_subject(msg)
+            else:
+                raise UnknownActionError(
+                    "Don't know how to handle action '{0}'.".format(msg.action))
+        except Exception as ex:
+            log.warn("Error provisioning message: {error}", error=ex)
+            raise
+
+    def get_config_defaults(self):
+        return dedent("""\
+            [PROVISIONER]
+            url_prefix = https://api.orgsync.com/api/v2
+            """)
 
     def parse_message(self, msg):
+        """
+        Parse message into a standard form.
+        """
+        doc = json.parse(msg)
+        action = doc['action']
+        subject = doc['subject']
+        attributes = None
+        if action  != DELETE_ACTION:
+            attributes = doc['attributes']
+        return ParsedMessage(action, subject, attributes)
+
+    @inlineCallbacks
+    def provision_subject(self, msg):
+        """
+        Provision a subject to Orgsync.
+        """
+        # Check if subject exists.
+        # Add or update subject record via API.
         pass
 
-            
+    @inlineCallbacks
+    def deprovision_subject(self, msg):
+        """
+        Deprovision a subject from Orgsync.
+        """
+        # Look up subject's Orgsync ID.
+        # Remove subject from Orgsync.
+        pass
 
