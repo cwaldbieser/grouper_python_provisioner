6c9dda97a57adaf0c1c487dd846a2fb76fc03d83
Carl Waldbieser <waldbiec@lafayette.edu>
2016-12-07 16:15:30 -0500

    Initial Board Effect provisioner.

---
+++ provisioner/txgroupprovisioner/board_effect_provisioner.py
@@ -0,0 +1,479 @@
+
+from __future__ import print_function
+from collections import namedtuple
+import datetime
+import json
+import commentjson
+import jinja2 
+from textwrap import dedent
+import attr
+import pylru
+import treq
+from twisted.internet import defer
+from twisted.internet.defer import (
+    inlineCallbacks, 
+    returnValue,
+)
+from twisted.internet.endpoints import clientFromString, connectProtocol
+from twisted.internet.task import LoopingCall
+from twisted.web.client import(
+    Agent,
+    HTTPConnectionPool,
+)
+from twisted.web.iweb import (
+    IAgentEndpointFactory,
+    IBodyProducer,
+)
+from twisted.logger import Logger
+from twisted.plugin import IPlugin
+from zope.interface import implements, implementer
+from config import load_config, section2dict
+import constants
+from errors import (
+    OptionMissingError,
+)
+from interface import (
+    IProvisionerFactory,
+    IProvisioner,
+)
+from utils import get_plugin_factory
+
+
+@attr.attrs
+class ParsedMessage(object):
+    action = attr.attrib()
+    subject = attr.attrib()
+    attributes = attr.attrib(default=attr.Factory(list))
+
+
+class UnknowActionError(Exception):
+    pass
+
+
+@implementer(IAgentEndpointFactory)
+class WebClientEndpointFactory(object):
+    """
+    An Agent endpoint factory based on endpoint strings.
+    """
+    def __init__(self, reactor, endpoint_s):
+        self.reactor = reactor
+        self.endpoint_s = endpoint_s
+
+    def endpointForURI(self, uri):
+        return clientFromString(self.reactor, self.endpoint_s)
+
+
+class StringProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return defer.succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
+
+
+class BoardEffectProvisionerFactory(object):
+    implements(IPlugin, IProvisionerFactory)
+    tag = "board_effect"
+    opt_help = "Board Effect RESTful API Provisioner"
+    opt_usage = "This plugin does not support any options."
+
+    def generateProvisioner(self, argstring=""):
+        """
+        Create an object that implements IProvisioner
+        """
+        provisioner = BoardEffectProvisioner()
+        return provisioner
+
+
+class BoardEffectProvisioner(object):
+    implements(IProvisioner)
+    service_state = None
+    reactor = None
+    log = None
+
+    def load_config(self, config_file, default_log_level, logObserverFactory):
+        """                                                             
+        Load the configuration for this provisioner and initialize it.  
+        """             
+        log = Logger(observer=logObserverFactory("ERROR"))
+        try:
+            # Load config.
+            scp = load_config(config_file, defaults=self.get_config_defaults())
+            section = "PROVISIONER"
+            config = section2dict(scp, section)
+            self.config = config
+            # Start logger.
+            log_level = config.get('log_level', default_log_level)
+            log = Logger(observer=logObserverFactory(log_level))
+            self.log = log
+            log.info("Initializing provisioner.",
+                event_type='init_provisioner')
+            # Load API configuration info-- endpoint info, URL, API key.
+            try:
+                self.diagnostic_mode = bool(config.get("diagnostic_mode", False))
+                self.endpoint_s = config.get("endpoint", None)
+                self.url_prefix = config["url_prefix"]
+                self.api_key = config["api_key"]
+                self.cache_size = int(config["cache_size"])
+                self.authenticate = config['authenticate']
+                self.accounts_query = config['accounts_query']
+                self.account_update = jinja2.Template(config['account_update'])
+                self.account_delete = jinja2.Template(config['account_delete'])
+                self.account_add = jinja2.Template(config['account_add'])
+                account_template_path = config['account_template']
+                attrib_map_path = config["attribute_map"]
+            except KeyError as ex:
+                raise OptionMissingError(
+                    "A require option was missing: '{0}:{1}'.".format(
+                        section, ex.args[0]))
+            # Create the web client.
+            self.make_web_client()
+            # Create the attribute map.
+            self.make_attribute_map(attrib_map_path)
+            # Create the account template.
+            self.make_account_template(account_template_path)
+            # Create account cache.
+            self.__account_cache = pylru.lrucache(self.cache_size)
+            # Initialize access token.
+            self.__auth_token = None
+        except Exception as ex:
+            d = self.reactor.callLater(0, self.reactor.stop)
+            log.failure("Provisioner failed to initialize: {0}".format(ex))
+            raise
+        return defer.succeed(None)
+
+    def make_attribute_map(self, path):
+        with open(path, "r") as f:
+            doc = commentjson.load(f)
+        self.attribute_map = {}
+        for k, v in doc.items():
+            try:
+                self.attribute_map[k.lower()] = jinja2.Template(v)
+            except jinja2.exceptions.TemplateError as ex:
+                log.error(
+                    "Error parsing attribute template for '{attribute}'",
+                    attribute=k)
+                raise
+                     
+    def make_account_template(self, path):
+        with open(path, "r") as f:
+            data = f.read()
+        self.account_template = jinja2.Template(data)
+
+    @inlineCallbacks                                                   
+    def provision(self, amqp_message):             
+        """                                                
+        Provision an entry based on an AMQP message.  
+        """                                              
+        log = self.log
+        try:
+            msg = self.parse_message(amqp_message)
+        except Exception as ex:
+            log.warn("Error parsing message: {error}", error=ex)
+            raise
+        try:
+            if msg.action in (constants.ACTION_ADD, constants.ACTION_UPDATE):
+                yield self.provision_subject(msg)
+            elif msg.action == constants.ACTION_DELETE:
+                yield self.deprovision_subject(msg)
+            else:
+                raise UnknownActionError(
+                    "Don't know how to handle action '{0}'.".format(msg.action))
+        except Exception as ex:
+            log.warn("Error provisioning message: {error}", error=ex)
+            raise
+
+    def get_config_defaults(self):
+        return dedent("""\
+            [PROVISIONER]
+            diagnostic_mode = 0
+            url_prefix = https://lafayette.boardeffect.com/api/v3
+            cache_size = 1000
+            """)
+
+    def parse_message(self, msg):
+        """
+        Parse message into a standard form.
+        """
+        serialized = msg.content.body
+        doc = json.loads(serialized)
+        action = doc['action']
+        subject = doc['subject']
+        attributes = None
+        if action  != constants.ACTION_DELETE:
+            attributes = doc['attributes']
+        return ParsedMessage(action, subject, attributes)
+
+    @inlineCallbacks
+    def provision_subject(self, msg):
+        """
+        Provision a subject to Board Effect.
+        """
+        log = self.log
+        log.debug(
+            "Attempting to provision subject '{subject}'.",
+            subject=msg.subject)
+        account = yield self.fetch_existing_account(msg)
+        if account is not None:
+            yield self.update_subject(account, msg)
+        else:
+            yield self.add_subject(msg)
+        returnValue(None)
+
+    @inlineCallbacks
+    def make_authenticated_api_call(self, method, url, **http_options):
+        http_client = self.http_client
+        auth_token = self.__auth_token
+        if auth_token is None:
+            prefix = self.url_prefix
+            url = "{0}{1}".format(
+                prefix,
+                self.authenticate 
+            )
+            headers = {
+                'Accept': ['application/json'],
+            }
+            api_key = self.api_key
+            params = dict(api_key=api_key)
+            response = yield http_client.get(url, headers=headers, params=params)
+            resp_code = response.code
+            if resp_code == 200:
+                try:
+                    doc = yield resp.json()
+                except Exception as ex:
+                    log.error("Error attempting to parse response to authentication request.")
+                    raise
+                if not "data" in doc:
+                    log.error("Error attempting to parse response to authentication request.")
+                    raise Exception("Error parsing authentication response.")
+                data = doc["data"]
+                if not "token" in data:
+                    log.error("Error attempting to parse response to authentication request.")
+                    raise Exception("Error parsing authentication response.")
+                self.__auth_token = data["token"]
+                auth_token =  self.__auth_token
+        headers = http_options.setdefault("headers", {})
+        headers["Authorization"] = [auth_token]
+        method = method.lower()
+        response = yield getattr(http_client, method)(url, **http_options)
+        returnValue(response)
+
+    @inlineCallbacks
+    def fetch_all_users(self):
+        """
+        Load all remote user accounts from the sevice.
+        """
+        http_client = self.http_client
+        prefix = self.url_prefix
+        url = "{0}{1}".format(prefix, self.accounts_query)
+        headers = {
+            'Accept': ['application/json'],
+        }
+        log.debug("URL (GET): {url}", url=url)
+        log.debug("headers: {headers}", headers=headers)
+        try:
+            resp = yield self.make_authenticated_api_call("GET", url, headers=headers, params=params)
+        except Exception as ex:
+            log.error("Error attempting to retrieve existing account.")
+            raise
+        resp_code = resp.code
+        if resp_code != 200:
+            raise Exception("Invalid response code: {0}".format(resp_code))
+        try:
+            doc = yield resp.json()
+        except Exception as ex:
+            log.error("Error attempting to parse response.")
+            raise
+        log.debug(
+            "Received response: {response}",
+            response=doc)
+        returnValue(doc)
+
+    @inlineCallbacks
+    def fetch_account_id(self, msg):
+        """
+        Fetch an existing remote account ID and return it or None if the remote 
+        account does not exist.
+        """
+        log = self.log
+        log.debug("Attempting to fetch existing account.")
+        account_cache = self.__account_cache
+        cache_size = self.cache_size
+        account_data = None
+        if len(account_cache) < cache_size:
+            # Prefill cache.
+            doc = yield self.fetch_all_users()
+            account_data = response["data"]
+            for n, entry in data: 
+                if n >= cache_size:
+                    break
+                login = entry["login"]
+                identifier = entry["id"]
+                account_cache[login] = identifier 
+        subject = msg.subject.lower()
+        if subject in account_cache:
+            remote_id = account_cache[subject]
+            returnValue(remot
+        if account_data is None:
+            account_data = yield self.fetch_all_users()
+        for entry in data:
+            if entry["login"].lower() == subject:
+                remote_id = entry["id"]
+                account_cache[subject] = remote_id
+                returnValue(remote_id)
+        returnValue(None)
+
+    @inlineCallbacks
+    def update_subject(self, account, msg):
+        """
+        Update a remote account.
+        """
+        log = self.log
+        subject = msg.subject
+        log.debug("Updating subject '{subject}'", subject=subject)
+        attributes = msg.attributes
+        props = self.map_attributes(attributes, subject, msg.action)
+        http_client = self.http_client
+        prefix = self.url_prefix
+        url = "{0}{1}".format(
+            prefix,
+            self.account_update.render(
+                account=account,
+                subject=msg.subject,
+                attributes=props))
+        params = {'key': self.api_key}
+        params.update(props)
+        headers = {'Accept': ['application/json']}
+        log.debug("url: {url}", url=url)
+        log.debug("headers: {headers}", headers=headers)
+        log.debug("params: {params}", params=params)
+        if not self.diagnostic_mode:
+            try:
+                resp = yield http_client.put(url, headers=headers, params=params)
+            except Exception as ex:
+                log.error("Error attempting to update existing account.")
+                raise
+            resp_code = resp.code
+            log.debug("Response code: {code}", code=resp_code)
+            yield resp.content()
+
+    def map_attributes(self, attribs, subject, action):
+        """
+        Map subject attributes to OrgSync attributes.
+        Returns OrgSync attributes mapping.
+        """
+        attrib_map = self.attribute_map
+        props = {}
+        for prop_name, template in attrib_map.items():
+            value = template.render(
+                subject=subject,
+                attributes=attribs,
+                action=action)
+            if value != u'\x00':
+                props[prop_name] = value
+        return props
+
+    @inlineCallbacks
+    def add_subject(self, msg):
+        """
+        Add an Orgsync account.
+        """
+        log = self.log
+        if False:
+            yield None
+        log.debug("Adding a new account.")
+        attributes = msg.attributes
+        props = self.map_attributes(attributes, subject, msg.action)
+        account_doc = self.account_template.render(
+            attributes=props,
+            subject=msg.subject,
+            action=msg.action)
+        log.debug("Account doc: {doc}", doc=account_doc)
+        http_client = self.http_client
+        prefix = self.url_prefix
+        url = "{0}{1}".format(
+            prefix,
+            self.account_add.render(
+                account=json.dumps(account_doc),
+                subject=msg.subject))
+        params = {'key': self.api_key}
+        headers = {
+            'Accept': ['application/json'], 
+            'Content-Type': ['application/json']}
+        log.debug("url: {url}", url=url)
+        log.debug("headers: {headers}", headers=headers)
+        log.debug("params: {params}", params=params)
+        if not self.diagnostic_mode:
+            try:
+                resp = yield http_client.post(
+                    url, 
+                    data=StringProducer(account_doc.encode('utf-8')), 
+                    headers=headers, 
+                    params=params)
+            except Exception as ex:
+                log.error("Error attempting to add new account.")
+                raise
+            resp_code = resp.code
+            log.debug("Response code: {code}", code=resp_code)
+            yield resp.content()
+
+    @inlineCallbacks
+    def deprovision_subject(self, msg):
+        """
+        Deprovision a subject from Orgsync.
+        """
+        log = self.log
+        log.debug(
+            "Attempting to deprovision subject '{subject}'.",
+            subject=msg.subject)
+        account = yield self.fetch_existing_account(msg)
+        if account is None:
+            log.debug("Account already does not exist.")
+            returnValue(None)
+        http_client = self.http_client
+        prefix = self.url_prefix
+        url = "{0}{1}".format(
+            prefix,
+            self.account_delete.render(
+                account=account,
+                subject=msg.subject))
+        params = {'key': self.api_key}
+        headers = {
+            'Accept': ['application/json']}
+        log.debug("url: {url}", url=url)
+        log.debug("headers: {headers}", headers=headers)
+        log.debug("params: {params}", params=params)
+        if not self.diagnostic_mode:
+            try:
+                resp = yield http_client.delete(url, headers=headers, params=params)
+            except Exception as ex:
+                log.error("Error attempting to delete existing account.")
+                raise
+            resp_code = resp.code
+            log.debug("Response code: {code}", code=resp_code)
+            yield resp.content()
+
+    def make_web_agent(self):
+        """
+        Configure a `Twisted.web.client.Agent` to be used to make REST calls.
+        """
+        self.pool = HTTPConnectionPool(self.reactor)
+        self.agent = Agent.usingEndpointFactory(
+            self.reactor,
+            WebClientEndpointFactory(self.reactor, self.endpoint_s),
+            pool=self.pool)
+
+    def make_web_client(self):
+        self.make_web_agent()
+        self.http_client = treq.client.HTTPClient(self.agent)
+
