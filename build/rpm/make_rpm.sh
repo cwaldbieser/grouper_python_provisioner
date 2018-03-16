78e6df713e06729a6e033ad255d6cc669537f84c
Carl Waldbieser <waldbiec@lafayette.edu>
2018-03-16 16:13:38 -0400

    Build scripts for RPM package.

---
+++ build/rpm/make_rpm.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+THISDIR="$( cd $(dirname $0); pwd)"
+SCRIPT="$THISDIR/$(basename $0)"
+ITERATION=${ITERATION:-1}
+FPM=/usr/local/bin/fpm
+OS_DEPS='jython >= 2.7.0'
+RPM_STEM="pychangelogger"
+SOFTWARE=$(readlink -f "/opt/pychangelogger")
+WEBROOT=$(readlink -f "/var/www/lutil")
+AFTER_INSTALL="$THISDIR/post-install.sh"
+OUTDIR=${OUTDIR:-/var/local/rpms}
+
+function get_version
+{
+    pushd "$WEBROOT" > /dev/null
+    git describe --abbrev=0 --tags
+    popd > /dev/null
+}
+VERSION=$(get_version)
+
+pushd "$OUTDIR" > /dev/null
+"$FPM" \
+    --verbose -s dir -t rpm -d "$OS_DEPS" -n "$RPM_STEM" \
+    -v "$VERSION" --iteration "$ITERATION" \
+    -x '*.pyc' -x '*.pem' -x '*.pid' -x '*.log' -x '*.gitignore' \
+    -x '*.cfg' -x '*.tgz' -x '*.zip' -x '*.tar.gz' -x '*.pkc' \
+    -x '*.log' \
+    --exclude '*/.git' \
+    --rpm-use-file-permissions \
+    --after-install "$AFTER_INSTALL" \
+    -p "$OUTDIR" \
+    "$SOFTWARE"
+popd > /dev/null
+
---
+++ build/rpm/post-install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Application log folder.
+mkdir -p /var/log/pychangelogger
+mkdir -p /etc/pychangelogger
+mkdir -p /var/run/pychangelogger/jython-cache
